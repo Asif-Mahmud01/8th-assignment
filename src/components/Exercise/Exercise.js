baedc93dfdba2d19de5ae839a22118cac1e65f08
Asif-Mahmud01 <asifmahmud3.am@gmail.com>
2022-09-29 21:12:35 +0600

    third commit

---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,16 @@
+.gymnasium-container{
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+}
+.exercises-container h2{
+    color: #82B6D9;
+}
+.exercises-container{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 30px;
+    padding: 50px;
+}
+.list-container{
+    margin-right: 50px;
+}
